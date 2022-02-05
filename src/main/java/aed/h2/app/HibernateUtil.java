1166c1b7fd929aa1c46f7b4095405d5da345e7d2
PuntunzOfficial <puntunzofficial@gmail.com>
2022-02-05 18:06:54 +0000

    1

--- src/main/java/aed/h2/app/HibernateUtil.java
+++ src/main/java/aed/h2/app/HibernateUtil.java
@@ -7,29 +7,37 @@ import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 public class HibernateUtil {
-	  private static final SessionFactory sessionFactory = buildSessionFactory();
+    private static StandardServiceRegistry registry;
+    private static SessionFactory sessionFactory;
 
-	    private static SessionFactory buildSessionFactory() {
-	        try {
-	            // Create the SessionFactory from hibernate.cfg.xml
-	            StandardServiceRegistry standardRegistry = new StandardServiceRegistryBuilder() 
-	            												.configure("hibernate.cfg.xml")
-	            												.build();
-	            Metadata metadata = new MetadataSources(standardRegistry)
-	            		.getMetadataBuilder()
-	            		.build();
-	            return metadata
-	            		.getSessionFactoryBuilder()
-	            		.build();
-	        } 
-	        catch (Throwable ex) {
-	            // Make sure you log the exception, as it might be swallowed
-	            System.err.println("Initial SessionFactory creation failed." + ex);
-	            throw new ExceptionInInitializerError(ex);
-	        }
-	    }
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                // Create registry
+                registry = new StandardServiceRegistryBuilder().configure().build();
 
-	    public static SessionFactory getSessionFactory() {
-	        return sessionFactory;
-	    }
+                // Create MetadataSources
+                MetadataSources sources = new MetadataSources(registry);
+
+                // Create Metadata
+                Metadata metadata = sources.getMetadataBuilder().build();
+
+                // Create SessionFactory
+                sessionFactory = metadata.getSessionFactoryBuilder().build();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                if (registry != null) {
+                    StandardServiceRegistryBuilder.destroy(registry);
+                }
+            }
+        }
+        return sessionFactory;
+    }
+
+    public static void shutdown() {
+        if (registry != null) {
+            StandardServiceRegistryBuilder.destroy(registry);
+        }
+    }
 }
